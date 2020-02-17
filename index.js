668c1f9fbd4baa155409c618a1819694350de3d9
ajouaouomar <omar_loka@hotmail.com>
2020-02-17 15:56:50 +0100

    create Redis class

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('hello')
+import redis from 'ioRedis'
+
+let clients = {}
+
+export default class Redis {
+    /**
+     * 
+     * @param {array} options 
+     * @param {string} name 
+     */
+    static configure(options, name = 'default') {
+        return new Promise((resolve, reject) => {
+            clients[name] = redis.Cluster(options)
+            clients[name].on('connect', () => {
+                resolve(clients[name])
+            })
+            clients[name].on('error', error => {
+                reject(error)
+            })
+        })
+    }
+    static get client() {
+        return this.getClient()
+    }
+    static getClient(name = 'default') {
+        return clients[name]
+    }
+}
